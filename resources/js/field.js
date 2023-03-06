19dec8ca0235572039ede4af12ca2d346efece5b
Kevin BENAROCHE <kevin.benaroche@hotmail.fr>
2023-03-06 23:31:22 +0200

    update dist

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,7 +1,7 @@
 Nova.booting(Vue => {
-    Vue.component('index-nova-inline-relationship', require('./components/IndexField').default);
-    Vue.component('detail-nova-inline-relationship', require('./components/DetailField').default);
-    Vue.component('form-nova-inline-relationship', require('./components/FormField').default);
+    Vue.component('index-nova-inline-relationship-extended', require('./components/IndexField').default);
+    Vue.component('detail-nova-inline-relationship-extended', require('./components/DetailField').default);
+    Vue.component('form-nova-inline-relationship-extended', require('./components/FormField').default);
 
     Vue.config.devtools = false;
 });
